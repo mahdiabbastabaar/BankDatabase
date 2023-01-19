0c5643734bd153c6cc6935c5a9adaabee77dece1
Mehdi Abbastabaar <m.abbastabaar@digikala.com>
2023-01-19 19:37:41 +0330

    added the migration

--- model/model.go
+++ model/model.go
@@ -1,1 +1,24 @@
 package model
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const DSN = "host=postgres user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Tehran"
+
+func InitialMigration() {
+	db, err := gorm.Open(postgres.Open(DSN), &gorm.Config{})
+	DB = db
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	err = db.AutoMigrate(&User{}, &Vote{}, &Comment{}, &Label{}, &Issue{}, &File{})
+	if err != nil {
+		return
+	}
+}
