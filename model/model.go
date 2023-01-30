ca758445fe3f4481ae202d72c309cae7c6eaeb4a
Mehdi Abbastabaar <m.abbastabaar@digikala.com>
2023-01-31 02:07:17 +0330

    improvement: added some entities

--- model/model.go
+++ model/model.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"time"
 )
 
 const DSN = "host=postgres user=admin password=1234 dbname=bank port=5432 sslmode=disable TimeZone=Asia/Tehran"
@@ -13,12 +14,12 @@ var DB *gorm.DB
 type Customer struct {
 	gorm.Model
 
-	NationalId int     `json:"national_id"`
+	NationalId int     `json:"national_id" gorm:"primaryKey"`
 	Address    string  `json:"address"`
-	FirstName  string  `json:"first_name"`
-	LastName   string  `json:"last_name"`
-	Sex        string  `json:"sex"`
-	Age        int     `json:"age"`
+	FirstName  string  `json:"first_name" gorm:"not null"`
+	LastName   string  `json:"last_name" gorm:"not null"`
+	Sex        string  `json:"sex" gorm:"check:sex in ('male', 'female');not null"`
+	Age        int     `json:"age" gorm:"not null"`
 	Credit     float64 `json:"credit"`
 }
 
@@ -26,7 +27,47 @@ type BusinessPlan struct {
 	gorm.Model
 
 	Description     string `json:"description"`
-	DiscountPercent int    `gorm:"check:discount_percent <= 100" json:"discount_percent"`
+	DiscountPercent int    `gorm:"check:discount_percent <= 100;not null" json:"discount_percent"`
+}
+
+type BusinessCustomer struct {
+	gorm.Model
+
+	NationalId   int          `json:"nationalId" gorm:"primaryKey"`
+	BPId         int          `json:"bpid"`
+	Customer     Customer     `gorm:"foreignKey:NationalId;references:NationalId"`
+	BusinessPlan BusinessPlan `gorm:"foreignKey:BPId;references:ID"`
+}
+
+type Report struct {
+	gorm.Model
+
+	OccurrenceTime time.Time `gorm:"not null"`
+}
+
+type Employee struct {
+	gorm.Model
+
+	Salary      float64 `gorm:"not null"`
+	EmpPassword string  `gorm:"not null"`
+	FirstName   string  `json:"first_name" gorm:"not null"`
+	LastName    string  `json:"last_name" gorm:"not null"`
+	Sex         string  `json:"sex" gorm:"check:sex in ('male', 'female');not null"`
+	Age         int     `json:"age" gorm:"not null"`
+}
+
+type StorageHallEmployee struct {
+	SHEId    int      `json:"she_id" gorm:"primaryKey"`
+	Employee Employee `gorm:"foreignKey:SHEId;references:ID"`
+}
+
+type StorageHall struct {
+	gorm.Model
+
+	CameraNumbers       int                 `json:"camera_numbers" gorm:"default:0"`
+	WallQuality         int                 `json:"wall_quality" gorm:"check:wall_quality in(1, 2, 3);not null"`
+	SHEId               int                 `json:"she_id"`
+	StorageHallEmployee StorageHallEmployee `gorm:"foreignKey:SHEId;references:SHEId"`
 }
 
 func InitialMigration() {
