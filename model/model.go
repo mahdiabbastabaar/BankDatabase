0082224eed99f19d188129a1c76b945906e4128f
Ali Rahmizad <79265219+AliAgha888@users.noreply.github.com>
2023-01-31 03:58:32 +0330

    Improvement: Completed entities.

--- model/model.go
+++ model/model.go
@@ -35,8 +35,8 @@ type BusinessCustomer struct {
 
 	NationalId   int          `json:"nationalId" gorm:"primaryKey"`
 	BPId         int          `json:"bpid"`
-	Customer     Customer     `json:"customer" gorm:"foreignKey:NationalId;references:NationalId"`
-	BusinessPlan BusinessPlan `json:"business_plan" gorm:"foreignKey:BPId;references:ID"`
+	Customer     Customer     `gorm:"foreignKey:NationalId;references:NationalId"`
+	BusinessPlan BusinessPlan `gorm:"foreignKey:BPId;references:ID"`
 }
 
 type Report struct {
@@ -74,8 +74,76 @@ type Account struct {
 	gorm.Model
 
 	Balance  float64  `json:"balance" gorm:"default:0"`
-	CUId     int      `json:"cu-id"`
+	CUId     int      `json:"cu_id"`
 	Customer Customer `gorm:"foreignKey:CUId;references:NationalId"`
+}
+
+type SafeBox struct {
+	gorm.model
+
+	MaximumValue float64     `json:"maximum_value" gorm:"not null"`
+	CUId         int         `json:"cu_id"`
+	SHId         int         `josn:"sh_id"`
+	priceClass   int         `json:"price_class" gorm:"not null"`
+	Customer     Customer    `gorm:"foreignKey:CUId;references:NationalId"`
+	StorageHall  StorageHall `gorm:"foreignKey:SHId;references:ID"`
+}
+
+type Rent struct {
+	gorm.Model
+
+	REId     int      `json:"re_id" gorm:"unique;not null"`
+	CUId     string   `json:"cu_id" gorm:"primaryKey"`
+	SBId     int      `json:"sb_id" gorm:"primaryKey"`
+	Customer Customer `gorm:"foreignKey:CUId;references:NationalId"`
+	SafeBox  SafeBox  `gorm:"foreignKey:SBId;references:ID`
+}
+
+type Contract struct {
+	gorm Model
+
+	baseAmount      float64   `json:"base_amount" gorm:"not null"`
+	fromTime        time.Time `json:"from_time" gorm:"not null"`
+	toTime	        time.Time `json:"to_time" gorm:"not null"`
+	duration        time.Time `json:"duration" gorm:"not null"`
+	discountPercent	int       `json:"discount_percent" gorm:"check:discountPercent <= 100;default:0"`
+	REId            int       `json:"re_id"`
+	Rent            Rent      `gorm:"foreignKey:REId;references:REId"`
+}
+
+type Services struct {
+	gorm.Model
+
+	COId        int      `json:"co_id" gorm:"primaryKey"`
+	serviceName string   `json:"service_name" gorm:"primaryKey;not null"`
+	serviceCost float64  `json:"service_cost" gorm:"not null"`
+	Contract    Contract `gorm:"foreignKey:COId;references:COId"`
+}
+
+type DamageReport {
+	gorm.Model
+
+	RId                 int                 `json:"r_id"`
+	damageMeasure       float64             `json:"damage_measure" gorm:"not null"`
+	damageDescription   string              `json:"damage_description"`
+	SHEId               int                 `json:"she_id"`
+	REId                int                 `json:"re_id"`
+	Report              Report              `gorm:"foreignKey:RId;references:ID"`
+	StorageHallEmployee StorageHallEmployee `gorm:"foreignKey:SHEId;references:SHEId"`
+	Rent                Rent                `gorm:"foreignKey:REId;references:REId"`
+}
+
+type EvacuationReport struct {
+	gorm.Model
+
+	RId                 int                 `json:"r_id"`
+	checkAmount         float64             `json:"check_amount" gorm:"not null"`
+	checkStatus         string              `json:"check_status" gorm:"check:check_status in ('Done', 'Not Done');not null"`
+	SHEId               int                 `json:"she_id"`
+	REId                int                 `json:"re_id"`
+	Report              Report              `gorm:"foreignKey:RId;references:ID"`
+	StorageHallEmployee StorageHallEmployee `gorm:"foreignKey:SHEId;references:SHEId"`
+	Rent                Rent                `gorm:"foreignKey:REId;references:REId"`
 }
 
 func InitialMigration() {
